807f67474ff7919ccbb96b26a6b0142f48f98f0d
john-emerson14 <john.carv.sousa@gmail.com>
2020-05-04 15:50:14 -0300

    feat: add persons widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:movie_app/style/theme.dart' as Style;
 import 'package:movie_app/widgets/genres.dart';
 import 'package:movie_app/widgets/now_playing.dart';
+import 'package:movie_app/widgets/persons.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -36,6 +37,7 @@ class _HomeScreenState extends State<HomeScreen> {
         children: <Widget>[
           NowPlaying(),
           GenresScreen(),
+          PersonsList(),
         ],
       ),
     );
